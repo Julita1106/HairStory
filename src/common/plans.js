b77eba7ef61da04020a069c0984a51f000cc1917
Julita1106 <julita.k.gola@gmail.com>
2021-05-20 21:53:55 +0200

    moved plans

---
+++ src/common/plans.js
@@ -0,0 +1,141 @@
+export const plans = {
+	highpourous: {
+		week: 1,
+		isCompleted: true,
+		days: [
+			{
+				day: "Poniedzialek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Olejowanie na 15 min przed myciem olejem z pestek dyni",
+						isDone: true,
+						id: 1,
+					},
+					{
+						text: "Mycie: Sessio Mango",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Maska emolientowa: Garnier Fruictis, Macadamia oils",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Wcierka: Vianek",
+						isDone: false,
+						id: 4,
+					},
+					{
+						text: "Serum: Marion termo",
+						isDone: false,
+						id: 5,
+					},
+				],
+			},
+			{
+				day: "Wtorek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+
+			{
+				day: "Środa",
+				isCompleted: false,
+				tasks: [
+					{
+						text:
+							"Olejowanie całonocne olejem z pestek dyni na podkład aloesowy",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+
+			{
+				day: "Czwartek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Mycie: Sessio Mango",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Maska emolientowa: Garnier Fruictis, Macadamia oils",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Wcierka: Vianek",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Serum: Marion termo",
+						isDone: false,
+						id: 4,
+					},
+				],
+			},
+
+			{
+				day: "Piątek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: Vianek",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+			{
+				day: "Sobota",
+				isCompleted: false,
+				tasks: [
+					{
+						text:
+							"Olejowanie calonocne olejem z pestek dyni na podład aloesowy",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+			{
+				day: "Niedziela",
+				isCpmpleted: false,
+				tasks: [
+					{
+						text: "Mycie: Sessio Mango",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Maska emolientowa: Garnier Fruictis, Macadamia oils",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Wcierka: Vianek",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Serum: Marion termo",
+						isDone: false,
+						id: 4,
+					},
+				],
+			},
+		],
+	},
+	mediumpourous: {},
+};
