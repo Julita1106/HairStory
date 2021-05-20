c891ab8424d04117d7b68aeaae616a5bc2ca9b39
Julita1106 <julita.k.gola@gmail.com>
2021-05-20 21:52:51 +0200

    added google user data into context

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,22 +1,62 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
-import { clientId } from "utils/utils";
+import { history } from "common/utils";
+import { clientId } from "common/utils";
+import { Context } from "common/context";
 
 import "./Navbar.scss";
 
-const CustomButton = ({ onClick, disabled }) => (
+const CustomButton = ({ onClick, disabled, label }) => (
 	<button className='navbar__btn' onClick={onClick} disabled={disabled}>
-		Log in
+		{label}
 	</button>
 );
 
 export const Navbar = () => {
-	const response = (response) => console.log(response);
+	const [context, setContext] = React.useContext(Context);
+
+	const onLoginSuccess = (response) => {
+		setContext({ ...context, user: response.profileObj, isLogged: true });
+		history.push(`/${response.profileObj.googleId}/quiz`);
+	};
+
+	const onLogoutSuccess = () => setContext({});
+	const onFailure = (response) => console.log(response);
+
 	return (
 		<nav className='navbar'>
 			<p className='navbar__logo'>HairStory</p>
-			<GoogleLogin
+			{context.isLogged === true ? (
+				<GoogleLogout
+					clientId={clientId}
+					render={(renderProps) => (
+						<CustomButton
+							onClick={renderProps.onClick}
+							disabled={renderProps.disabled}
+							label={"Wyloguj"}
+						/>
+					)}
+					onLogoutSuccess={onLogoutSuccess}
+					onFailure={onFailure}
+					cookiePolicy={"single_host_origin"}
+				/>
+			) : (
+				<GoogleLogin
+					clientId={clientId}
+					render={(renderProps) => (
+						<CustomButton
+							onClick={renderProps.onClick}
+							disabled={renderProps.disabled}
+							label={"Zaloguj"}
+						/>
+					)}
+					onSuccess={onLoginSuccess}
+					onFailure={onFailure}
+					cookiePolicy={"single_host_origin"}
+				/>
+			)}
+			{/* <GoogleLogin
 				clientId={clientId}
 				render={(renderProps) => (
 					<CustomButton
@@ -25,10 +65,10 @@ export const Navbar = () => {
 						Log in
 					</CustomButton>
 				)}
-				onSuccess={response}
-				onFailure={response}
+				onSuccess={onSuccess}
+				onFailure={onFailure}
 				cookiePolicy={"single_host_origin"}
-			/>
+			/> */}
 		</nav>
 	);
 };
