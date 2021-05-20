c92e6e3c2a0cf9b84cb1d1e59c7fa200c2c7eaec
Julita1106 <julita.k.gola@gmail.com>
2021-05-20 20:37:43 +0200

    added router and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { history } from "util/util";
 import { Navbar } from "./components/navbar/Navbar";
 import { InitialPage } from "./components/initialPage/InitialPage";
 import { Footer } from "./components/footer/Footer";
 import { Container } from "./components/container/Container";
+import { QuizPage } from "./components/quizPage/QuizPage";
+import { PlanPage } from "./components/planPage/PlanPage";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Container>
-				<InitialPage />
+				<Router history={history}>
+					<Switch>
+						<Route path='/' exact component={InitialPage} />
+						<Route path='/:userId/quiz' component={QuizPage} />
+						<Route path='/:userId/plan' component={PlanPage} />
+					</Switch>
+				</Router>
 			</Container>
 			<Footer />
 		</>
