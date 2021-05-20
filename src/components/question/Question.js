dbdd73b8c2c89f9b29d552a0e2476ac4e875d8ce
Julita1106 <julita.k.gola@gmail.com>
2021-05-20 20:36:07 +0200

    added checked state

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -3,6 +3,10 @@ import React from "react";
 import "./Question.scss";
 
 export const Question = ({ question }) => {
+	const [value, setValue] = React.useState(null);
+
+	const onCheck = (e) => setValue(e.target.value);
+
 	return (
 		<div className='question'>
 			<div className='question__header'>
@@ -10,9 +14,17 @@ export const Question = ({ question }) => {
 			</div>
 			<div className='question__answers'>
 				{question.answers.map((answer) => {
+					const isChecked = value === answer.answerId;
+
 					return (
 						<div className='question__answer' key={answer.answerId}>
-							<input type='radio' value={answer.answerId} name={answer.text} />
+							<input
+								checked={isChecked}
+								onChange={onCheck}
+								type='radio'
+								value={answer.answerId}
+								name={answer.text}
+							/>
 							<label htmlFor={answer.text}>{answer.text}</label>
 						</div>
 					);
