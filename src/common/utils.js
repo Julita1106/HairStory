1484d1588c5bfc0c6d387283e476bbbdc35abd11
Julita1106 <julita.k.gola@gmail.com>
2021-05-20 21:54:29 +0200

    added router history

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,2 +1,6 @@
+import { createBrowserHistory } from "history";
+
+export const history = createBrowserHistory();
+
 export const clientId =
 	"479237004423-ch80tkr3h0m0hpjhjn7gkkabkjha39rq.apps.googleusercontent.com";
