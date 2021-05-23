c3ecc985d79d01c3a64587f1c46ca00329179979
Julita1106 <julita.k.gola@gmail.com>
2021-05-23 17:22:05 +0200

    checking if user is logged

--- src/components/quizPage/QuizPage.js
+++ src/components/quizPage/QuizPage.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 import { Quiz } from "components/quiz/Quiz";
+import { Context } from "common/context";
+import { history } from "common/utils";
 
 import "./QuizPage.scss";
 
 export const QuizPage = () => {
+	const [context] = React.useContext(Context);
+
+	if (!context.isLogged) {
+		history.push("/");
+		return null;
+	}
+
 	return (
 		<div className='quiz-page'>
 			<div className='quiz-page__heading'>
