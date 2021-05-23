0df758453f3203dad2c9e05311897a89e7990180
Julita1106 <julita.k.gola@gmail.com>
2021-05-23 17:12:25 +0200

    added more plans

--- src/common/plans.js
+++ src/common/plans.js
@@ -1,5 +1,5 @@
 export const plans = {
-	highpourous: {
+	highporous: {
 		week: 1,
 		isCompleted: true,
 		days: [
@@ -137,5 +137,300 @@ export const plans = {
 			},
 		],
 	},
-	mediumpourous: {},
+	mediumporous: {
+		week: 1,
+		isComplieted: true,
+		days: [
+			{
+				day: "Poniedzialek",
+				isCompleted: false,
+				tasks: [
+					{
+						text:
+							"Olejowanie na 30 min przed myciem olejem z awokado na podkład z panthenolu",
+						isDone: true,
+						id: 1,
+					},
+					{
+						text: "Mycie: Sessio Mango",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Peeling: Planeta Organica",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Maska Proteinowa: Kallos Milk",
+						isDone: false,
+						id: 4,
+					},
+					{
+						text: "Wcierka: Jantar",
+						isDone: false,
+						id: 5,
+					},
+					{
+						text: "Serum: Marion termo",
+						isDone: false,
+						id: 6,
+					},
+				],
+			},
+			{
+				day: "Wtorek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+
+			{
+				day: "Środa",
+				isCompleted: false,
+				tasks: [
+					{
+						text:
+							"Olejowanie całonocne olejem z pestek dyni na podkład aloesowy",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+
+			{
+				day: "Czwartek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Mycie: Sessio Mango",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Maska: Macadamia Natural Oil Deep",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Serum: Garnier, Goodbye Damage",
+						isDone: false,
+						id: 4,
+					},
+				],
+			},
+
+			{
+				day: "Piątek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: Vianek",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Płukanka: Yves Rocher-Malinowa",
+						isDone: false,
+						id: 2,
+					},
+				],
+			},
+			{
+				day: "Sobota",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: Jantar",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+			{
+				day: "Niedziela",
+				isCpmpleted: false,
+				tasks: [
+					{
+						text: "Mycie: Sessio Mango",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Maska humektantowa: Garnier Fruictis, Papaya",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Wcierka: Jantar",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Serum: Garnier Goodbye Damage",
+						isDone: false,
+						id: 4,
+					},
+				],
+			},
+		],
+	},
+
+	lowporous: {
+		week: 1,
+		isComplieted: true,
+		days: [
+			{
+				day: "Poniedzialek",
+				isCompleted: false,
+				tasks: [
+					{
+						text:
+							"Olejowanie na 30 min przed myciem olejem kokosowym na podkład z aloesu",
+						isDone: true,
+						id: 1,
+					},
+					{
+						text: "Mycie: Equilibra Aloes",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Peeling: Anven, kawowy",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Maska Emolientowa: Garnier Banana",
+						isDone: false,
+						id: 4,
+					},
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 5,
+					},
+					{
+						text: "Serum: Marion termo",
+						isDone: false,
+						id: 6,
+					},
+				],
+			},
+			{
+				day: "Wtorek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+
+			{
+				day: "Środa",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Olejowanie całonocne olejem kokosowym na podkład aloesowy",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+
+			{
+				day: "Czwartek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Mycie: Equilibra Aloes",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Maska Emolientowa: Macadamia Natural Oil Deep",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Serum: Marion termo",
+						isDone: false,
+						id: 4,
+					},
+				],
+			},
+
+			{
+				day: "Piątek",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Płukanka: octowo-jabłkowa",
+						isDone: false,
+						id: 2,
+					},
+				],
+			},
+			{
+				day: "Sobota",
+				isCompleted: false,
+				tasks: [
+					{
+						text: "Wcierka: czarnuszkowa",
+						isDone: false,
+						id: 1,
+					},
+				],
+			},
+			{
+				day: "Niedziela",
+				isCpmpleted: false,
+				tasks: [
+					{
+						text: "Mycie: Equilibra Aloes",
+						isDone: false,
+						id: 1,
+					},
+					{
+						text: "Maska proteinowa: Planeta Organica- trawa cytrynowa",
+						isDone: false,
+						id: 2,
+					},
+					{
+						text: "Wcierka: Jantar",
+						isDone: false,
+						id: 3,
+					},
+					{
+						text: "Serum: Marion termo",
+						isDone: false,
+						id: 4,
+					},
+				],
+			},
+		],
+	},
 };
